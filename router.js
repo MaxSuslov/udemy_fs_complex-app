e299951c13fe8f4c9e16657738c4bb05c361b37d
MaxSuslov <maxsusmail@gmail.com>
2021-11-16 18:20:31 +0100

    Created first controller, going to use MVC pattern and create a model in the next commit

--- router.js
+++ router.js
@@ -1,14 +1,9 @@
-// //// is executed immediately after require in app.js
-// console.log("I am executed immediately.")
-// //// is executed when the "router" variable is used where it was required, you can use router.name or router.meow()
-// module.exports = {name: "Meosalot", species: "cat", meow: function(){console.log("Meowwww!")}}
-
 const express = require('express')
 const router = express.Router()
+const userController = require('./controllers/userController')
 
-router.get('/', function(req, res) {
-  res.render('home-guest')
-})
+router.get('/', userController.home)
+router.post('/register', userController.register)
 
 
 module.exports = router
