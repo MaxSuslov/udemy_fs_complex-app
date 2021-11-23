f5fd9af5e7c40a3d447d8429b668f21df227bc71
Maksym Suslov <maxsusmail@gmail.com>
2021-11-23 20:18:04 +0100

    Redirecting the logged in user to the home page (home-dashboard for logged in users) usign .req.session.save() with a callback function inside to take pacing into account (timing for completing the request)

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,9 @@ exports.login = function(req, res) {
   let user = new User(req.body)
   user.login().then(function(result) {
     req.session.user = {favColor: "blue", username: user.data.username}
-    res.send(result)
+    req.session.save(function(){
+      res.redirect('/')
+    })
   }).catch(function(err) {
     res.send(err)
   })
