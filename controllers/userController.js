bf75890aa832aa97758117df63d3538ffc4d3dcc
Maksym Suslov <maxsusmail@gmail.com>
2021-11-23 22:43:39 +0100

    Errors fixed, checks for existing username and email during registration are working

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
+
 const User = require('../models/User')
 
 exports.login = function(req, res) {
+  // req.body is data from request posted via <form action="/login"> 
   let user = new User(req.body)
   user.login().then(function(result) {
     req.session.user = {favColor: "blue", username: user.data.username}
@@ -8,7 +10,10 @@ exports.login = function(req, res) {
       res.redirect('/')
     })
   }).catch(function(e) {
+    // 'errors' in flash is an array, the second arg is the message that you want to include in that array. You can add string of text or use params from .reject option of promise in User.js
     req.flash('errors', e)
+    // .flash() package is helping us to add/remove data from our session. It's adding a new object named "flash": {"errors":["Invalid username/password"]} into sessions
+    // flash object is being emptied after the first login attempt, so the user sees the error message only once, flash obj is just temporary storing error messages in session
     req.session.save(function() {
       res.redirect('/')
     })
@@ -32,9 +37,10 @@ exports.register = function(req, res) {
     regErrors.forEach(function(error) {
       req.flash('regErrors', error)
     })
+    // manually save the data and after it's completed, redirect user to the home page
     req.session.save(function() {
       res.redirect('/')
-    })
+    }) 
   })
 }
 
