42378c22326d5b615ea9af0a241034804be9f833
Maksym Suslov <maxsusmail@gmail.com>
2021-11-23 22:15:39 +0100

    User registration improvements attempt - checks for existing username and email - doesn't work as for now

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,19 +27,27 @@ exports.logout = function(req, res) {
 
 exports.register = function(req, res) {
   let user = new User(req.body)
-  user.register()
-  if (user.errors.length) {
-    res.send(user.errors)
-  } else {
-    res.send("Congrats, there are no errors.")
-  }
+  user.register().then(()=>{
+    req.session.user = {username: user.data.username}
+    req.session.save(function(){
+      res.redirect('/')
+    })
+  }).catch((regErrors)=>{
+    regErrors.forEach(function(error){
+      req.flash('regErrors', error)
+    })
+    // manually save the data and after it's completed, redirect user to the home page
+    req.session.save(function(){
+      res.redirect('/')
+    }) 
+  })
 }
 
 exports.home = function(req, res) {
   if (req.session.user) {
     res.render('home-dashboard', {username: req.session.user.username})
   } else {
-    res.render('home-guest', {errors: req.flash('errors')})
+    res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')})
   }
 }
  
