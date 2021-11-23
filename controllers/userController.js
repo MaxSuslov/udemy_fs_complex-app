6223218b0f65f8b3bb2613c6e81094263761b918
Maksym Suslov <maxsusmail@gmail.com>
2021-11-23 20:56:35 +0100

    Flash messages on failed login attempt - storing error messages temporary in session object in MongoDB and deleting them right after the first attempt to show them to user only once

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,13 @@ exports.login = function(req, res) {
       res.redirect('/')
     })
   }).catch(function(err) {
-    res.send(err)
+    // 'errors' in flash is an array, the second arg is the message that you want to include in that array. You can add string of text or use params from .reject option of promise in User.js
+    req.flash('errors', err)
+    // .flash() package is helping us to add/remove data from our session. It's adding a new object named "flash": {"errors":["Invalid username/password"]} into sessions
+    // flash object is being emptied after the first login attempt, so the user sees the error message only once, flash obj is just temporary storing error messages in session
+    req.session.save(function(){
+      res.redirect('/')
+    })
   })
 }
 
@@ -33,6 +39,7 @@ exports.home = function(req, res) {
   if (req.session.user) {
     res.render('home-dashboard', {username: req.session.user.username})
   } else {
-    res.render('home-guest')
+    res.render('home-guest', {errors: req.flash('errors')})
   }
 }
+ 
