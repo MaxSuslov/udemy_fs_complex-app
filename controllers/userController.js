d97c5fdd6994e719e327325bace34e596bc4fbf4
MaxSus <maxsusmail@gmail.com>
2022-02-02 21:11:02 +0100

    create-post template included, postController enriched, router adjusted. Reduced repetitive code (optimization of header and footer by sharing the same template chunks. Conditioning depending on session data (logged in or not) and showing different content upon that

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,17 @@
 
 const User = require('../models/User')
 
+exports.mustBeLoggedIn = function(req,res, next) {
+  if (req.session.user) {
+    next()
+  } else {
+    req.flash("errors", "You must be logged in to perform that action")
+    req.session.save(function() {
+      res.redirect('/')
+    })
+  }
+}
+
 exports.login = function(req, res) {
   // req.body is data from request posted via <form action="/login"> 
   let user = new User(req.body)
@@ -46,7 +57,7 @@ exports.register = function(req, res) {
 
 exports.home = function(req, res) {
   if (req.session.user) {
-    res.render('home-dashboard', {username: req.session.user.username, avatar: req.session.user.avatar})
+    res.render('home-dashboard')
   } else {
     res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')})
   }
--- router.js
+++ router.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('./controllers/userController')
+const postController = require('./controllers/postController')
 
+// user related routs
 router.get('/', userController.home)
 router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 
+// post related routs
+router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
 
 module.exports = router
