e6d77b90dc08415cc94002f31aa1ad1c18d624d1
MaxSus <maxsusmail@gmail.com>
2022-02-20 20:57:10 +0100

    404 page added. In single post view pull data from MongoDB to display user's avatar and username

--- models/Post.js
+++ models/Post.js
@@ -1,3 +1,7 @@
+const { ObjectID } = require('bson')
+const { default: isPostalCode } = require('validator/lib/isPostalCode')
+const User = require('./User')
+
 const postsCollection = require('../db').db().collection("posts")
 const ObjectId = require('mongodb').ObjectId
 
@@ -50,9 +54,30 @@ Post.findSingleById = function(id) {
       reject()
       return
     }
-    let post = await postsCollection.findOne({_id: new ObjectId(id)})
-    if (post) {
-      resolve(post)
+    let posts = await postsCollection.aggregate([
+      {$match: {_id: new ObjectID(id)}},
+      {$lookup: {from: "users", localField: "author", foreignField: "_id", as: "authorDocument"}},
+      {$project: {
+        title: 1,
+        body: 1,
+        createdDate: 1,
+        author: {$arrayElemAt: ["$authorDocument", 0]}
+      }}
+    ]).toArray()
+
+    // clean up author property in each post object
+    posts = posts.map(function(post) {
+      post.author = {
+        username: post.author.username,
+        avatar: new User(post.author, true).avatar
+      }
+      return post
+    })
+
+
+    if (posts.length) {
+      console.log(posts[0])
+      resolve(posts[0])
     } else {
       reject
     }
